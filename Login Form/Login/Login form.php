7f7d2e9569051f402459ceaed4ef989a73bb90d3
pranavkadam011 <164323472+pranavkadam011@users.noreply.github.com>
2024-03-21 14:12:38 +0530

    Add files via upload

---
+++ Login Form/Login/Login form.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>َShopeshoes/Login</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+<form class="box" action="C:\Users\PRANAV KADAM\Desktop\Shoes Website\index.html" method="post">
+  <h1>Login</h1>
+  <input type="text" name="" placeholder="Username">
+  <input type="password" name="" placeholder="Password">
+  <input type="submit" name="" value="Login">
+
+
+
+</form>
+
+
+
+<form class="box2" action="C:\Users\PRANAV KADAM\Desktop\Shoes Website\Login Form\New user\New User.html" method="post">
+  <h2>NEW USER</h2>
+  <input type="submit" name="" value="Register">
+
+  </body>
+</html>
---
+++ Login Form/New user/New User.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>َShopeshoes/َRegister</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+<form class="box" action="index.html" method="post">
+  <h1>REGISTER</h1>
+  <input type="text" name="" placeholder="First Name">
+ <input type="text" name="" placeholder="Last Name">
+ <input type="text" name="" placeholder="Email I'd">
+ <input type="text" name="" placeholder="Contact Number">
+  <input type="password" name="" placeholder="Password">
+  <input type="submit" name="" value="Submit">
+
+
+
+</form>
+
+  </body>
+</html>
